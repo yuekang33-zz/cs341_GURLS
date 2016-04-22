165d86e9c49ef4be4d4dd20ece5898c4f9a5a06c
ruixilin <rlin2@stanford.edu>
2016-04-21 23:05:57 -0700

    Added files via upload

--- graph.py
+++ graph.py
@@ -8,20 +8,23 @@ import dateutil.relativedelta
 pd.options.display.encoding = sys.stdout.encoding
 
 
-fp = codecs.open("file", "r", encoding='utf-8')
+fp = codecs.open("file.txt", "r", encoding='utf-8')
 
 
 
 df_reshare = pd.read_csv(fp,index_col=0,header = 0)
-df_reshare.columns = {"fdate_cd","bizuin_md5","appmsgid","itemids","url","len",”posttime”,”uin_md5”,”reshare_timestamp”,”pre_published”,”cur_published”,”cur_chatmember_count”}
-df_reshare = pd.DataFrame({'pre_published': df_reshare[‘pre_published’],'cur_published': df_reshare[‘cur_published’], ‘userId’: df_reshare[‘uin_md5’]});
+df_reshare.columns = {"fdate_cd","bizuin_md5","appmsgid","itemids","url","len","posttime","uin_md5","reshare_timestamp","pre_published","cur_published","cur_chatmember_count"}
+df_reshare = pd.DataFrame({'pre_published': df_reshare['pre_published'],'cur_published': df_reshare['cur_published'], 'userId': df_reshare['uin_md5']});
+print "before removing dup ", df_reshare
 df_reshare = df_reshare.drop_duplicates(keep = 'first')
+print df_reshare
 df_reshare = df_reshare.drop_duplicates(subset= 'cur_published', keep = 'first')
 
 
 preid = df_reshare['pre_published'].values.tolist();
 curid =df_reshare['cur_published'].values.tolist();
-userid =df_reshare['uin_md5'].values.tolist();
+userid =df_reshare['userId'].values.tolist();
+#print df_reshare
 
 
 
@@ -29,25 +32,27 @@ userid =df_reshare['uin_md5'].values.tolist();
 
 
 # convert userid to a node intergers
-#useridList = [0]*userid.length
-nonRepeatingUserid = []
-for elem in userid:
-	if elem is nonRepeatingUserid:
-		elem = nonRepeatingUserid.index(elem)
-	else:
-		nonRepeatingUserid.append(elem)
-elem = nonRepeatingUserid.index(elem)
+useridList = [0]*len(userid)
 print userid
+nonRepeatingUserid = []
+i = 0
+for elem in userid:
+  if elem is nonRepeatingUserid:
+    useridList[i] = nonRepeatingUserid.index(elem)
+  else:
+    nonRepeatingUserid.append(elem)
+    useridList[i] = nonRepeatingUserid.index(elem)
+  i += 1
+print useridList
 
 
 
 for id in preid:
-	if id not in curid:
-		id = -1
-	else:
-		id = userid(curid.index(id))
+  if id not in curid:
+    id = -1
+  else:
+    id = useridList(curid.index(id))
 print preid
 
-output = pd.DataFrame({“from”: preid, “to”: userid})
-newFrame.to_csv('output', sep=',')
-		
+output = pd.DataFrame({"from": preid, "to": userid})
+output.to_csv('output', sep=',')
